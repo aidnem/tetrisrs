48e2d1a32f7b035beedfe7f21970d32fbd23f795
aidnem <99768676+aidnem@users.noreply.github.com>
2025-05-15 18:07:02 -0400

    WIP - Start implementing tetris

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,45 @@
 use bevy::prelude::*;
 
-fn setup(
-    mut commands: Commands,
-) {
+#[derive(Component)]
+struct Piece {
+    x: u32,
+    y: u32,
+}
+
+#[derive(Component)]
+struct ActivePiece {}
+
+#[derive(Component)]
+struct NextPiece {}
+
+enum PieceKind {
+    I,
+    O,
+    T,
+    S,
+    Z,
+    J,
+    L,
+}
+
+fn setup(mut commands: Commands, mut meshes: ResMut<Assets<Mesh>>) {
     commands.spawn(Camera2d);
+
+    commands.spawn(());
 }
 
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins)
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                title: "Tetris".to_string(),
+                resizable: false,
+                resolution: (640., 480.).into(),
+                ..default()
+            }),
+            ..default()
+        }))
+        .insert_resource(ClearColor(Color::rgb(127.0, 127.0, 127.0)))
         .add_systems(Startup, setup)
         .run();
 }
